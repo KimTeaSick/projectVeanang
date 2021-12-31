a94a943e721e67d7315667fdd00d1c647084b9b6
kimteasick <notseph8556@gmail.com>
2021-12-31 23:17:50 +0900

    post폼 추가

--- front/pages/register.tsx
+++ front/pages/register.tsx
@@ -1,10 +1,38 @@
+import { Button, Form, Input } from 'antd';
+import axios from 'axios';
 import { NextPage } from 'next'
-import React,{FunctionComponent} from 'react'
+import React,{FunctionComponent, useCallback, useState} from 'react'
 
 const RegisterPage: NextPage = () => {
+  
+  let [email, setEmail] = useState('');
+  let [id, setId] = useState('');
+  let [password, setPassword] = useState('');
+
+
+  const emailHandler = useCallback((e: any) => {
+    setEmail(e.target.value);
+  },[])
+  const idHandler = useCallback((e: any) => {
+    setId(e.target.value);
+  },[])
+  const passwordHandler = useCallback((e: any) => {
+    setPassword(e.target.value);
+  },[])
+  const sendForm = useCallback(() =>{
+    axios.post('/api/login',{})
+  },[])
   return (
     <div>
-      
+      <Form>
+        <label>이메일</label>
+        <Input type="text" value={email} onChange={emailHandler}/>
+        <label>ID</label>
+        <Input type="text" value={id} onChange={idHandler}/>
+        <label>Password</label>
+        <Input type="text" value={password} onChange={passwordHandler}/>
+        <Button onClick={sendForm}>send</Button>
+      </Form>
     </div>
   )
 }
